ae7c6e59483f4b3b725b8d57b8257ca79603daef
user <iamsrk1165@gmail.com>
2025-01-22 15:28:34 +0500

    Created a class

--- course/models.py
+++ course/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    keywords = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
+
+class Subject(models.Model):
+    title = models.CharField(max_length=100)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    subject_tutor = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    price = models.CharField(max_length=255)
+    detail = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
